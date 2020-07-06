cd5581f942a5b9d62a11fa4594c484fc21cc163c
Jaso Salgado <jaso_98@hotmail.com>
2020-07-06 12:19:24 -0500

    <Fixed>: Ready to merge with master

--- backend/settings.py
+++ backend/settings.py
@@ -54,18 +54,16 @@ INSTALLED_APPS = [
     'quarto',
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
-    
+
     'DEFAULT_PERMISSION_CLASSES' : (
         'rest_framework.permissions.IsAuthenticated',),
+
     'DEFAULT_AUTHENTICATION_CLASSES' : (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',)
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',)
 }
 
 
--- backend/urls.py
+++ backend/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
+"""
+JWT endpoints: /api/token/ and /api/token/refresh/
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quarto.urls')),
-    
+
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view())
 ]
