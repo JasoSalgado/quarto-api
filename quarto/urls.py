2e6d1aca9769aa099f11427269c23dc32040c36b
Jaso Salgado <jaso_98@hotmail.com>
2020-07-06 12:16:53 -0500

    <Fixed>: I removed some endpoints

--- quarto/urls.py
+++ quarto/urls.py
@@ -1,31 +1,23 @@
 from django.urls import path, include
-from .views import user_list, user_detail, UserAPIView, UserDetail, UserGenericAPIView, UserViewSet, room_list, room_detail, RoomAPIView, RoomDetail, RoomGenericAPIView, RoomViewSet #SignUpView
+from .views import UserAPIView, UserDetail, UserViewSet, RoomAPIView, RoomDetail, RoomViewSet
 from rest_framework.routers import DefaultRouter
+
 
 router = DefaultRouter()
 router.register('user', UserViewSet, basename = 'user')
 router.register('room', RoomViewSet, basename = 'room')
 
 urlpatterns = [
-    # viewset urls
+    # viewset url
     path('viewset/', include(router.urls)),
-    path('viewset/<int:id>/', include(router.urls)),
-    
-    path('user/', user_list),
-    path('room/', room_list),
-    
+
     path('user/', UserAPIView.as_view()),
     path('room/', RoomAPIView.as_view()),
-    
+
     # UserDetail shows user detail
     path('user_detail/<int:id>/', UserDetail.as_view()),
     path('room_detail/<int:id>/', RoomDetail.as_view()),
-    
-    
-    # UserGenericAPIViews allows to create another user
-    path('generic/user/<int:id>/', UserGenericAPIView.as_view()),
-    path('generic/room/<int:id>/', RoomGenericAPIView.as_view()),
-    
+
 ]
 
 
